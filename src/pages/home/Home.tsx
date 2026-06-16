bfde7ca380d6176589a6ffe07ce5227d0cca66be
Your Name <you@example.com>
2026-06-16 19:35:08 +0300

    State

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import Counter from "../../widgets/counter/Counter";
+
+export default function Home() {
+
+    return <div className="container">
+        <h1>Hello, World!</h1>
+        <Counter/>
+        <Counter/>
+    </div>;
+}
