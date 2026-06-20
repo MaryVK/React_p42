5865678a528a9ba144b839a6ccede61482cfc353
Your Name <you@example.com>
2026-06-20 16:33:36 +0300

    saves

--- src/entities/group/api/GroupApi.ts
+++ src/entities/group/api/GroupApi.ts
@@ -19,6 +19,69 @@ export default class GroupApi {
                         description: "Shop Fashion for less",
                         slug: "jeans",
                         imageUrl: "/img/group2.jpg"
+                    },
+                    {
+                        id: "3",
+                        name: "Watches",
+                        description: "Wireless Tech",
+                        slug: "watches",
+                        imageUrl: "/img/group3.jpg"
+                    },
+                    {
+                        id: "4",
+                        name: "Smartphones",
+                        description: "Wireless Tech",
+                        slug: "smartphones",
+                        imageUrl: "/img/group4.jpg"
+                    },
+                    {
+                        id: "5",
+                        name: "Tablets",
+                        description: "Wireless Tech",
+                        slug: "tablets",
+                        imageUrl: "/img/group5.jpg"
+                    },
+                    {
+                        id: "6",
+                        name: "Headphones",
+                        description: "Wireless Tech",
+                        slug: "headphones",
+                        imageUrl: "/img/group6.jpg"
+                    },
+                    {
+                        id: "7",
+                        name: "PC",
+                        description: "Deals on top categories",
+                        slug: "pc",
+                        imageUrl: "/img/group7.jpg"
+                    },
+                    {
+                        id: "8",
+                        name: "Men",
+                        description: "Most-loved watches",
+                        slug: "men-watches",
+                        imageUrl: "/img/group8.jpg"
+                    },
+                    {
+                        id: "9",
+                        name: "Laptops",
+                        description: "Level up your PC here",
+                        slug: "laptops",
+                        imageUrl: "/img/group9.jpg"
+                    },
+                    {
+                        id: "10",
+                        name: "Equipment",
+                        description: "Gear up to get fit",
+                        slug: "equipment",
+                        imageUrl: "/img/group10.jpg"
+                    },
+                    {
+                        id: "11",
+                        name: "Kitchen & dining",
+                        description: "New home arrivals under $50",
+                        slug: "kitchen_dining",
+                        imageUrl: "/img/group11.jpg"
                     }
                 ]),
                 500
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Counter from "../../widgets/counter/Counter";
+import type IGroup from "../../entities/group/model/IGroup";
+import GroupApi from "../../entities/group/api/GroupApi";
 
 export default function Home() {
+    const [groups, setGroups] = useState<Array<IGroup>>([]);
+
+    useEffect(() => {
+        GroupApi.allGroups().then(setGroups);
+    }, []);
 
     return <div className="container">
-        <h1>Hello, World!</h1>
-        <Counter/>
-        <Counter/>
+        <h1>Крамниця</h1>
+
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+            {groups.map(g => <div className="col">
+                <div className="card h-100">
+                    <img src={g.imageUrl} className="card-img-top" alt={g.name}/>
+                    <div className="card-body">
+                        <h5 className="card-title">{g.name}</h5>
+                        <p className="card-text">{g.description}</p>
+                    </div>
+                </div>                
+            </div>)}
+        </div>
+        
+        
     </div>;
 }
